02d5a3c9848f1120c95e7a2dfea68148ee3dcfd2
Charles Viegas <cviegas@azi.net.br>
2018-06-19 10:44:42 -0400

    status progress in uploaded file

--- src/store.js
+++ src/store.js
@@ -7,7 +7,8 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
         token: null,
-        uploading: false
+        loading: false,
+        fileUploadProgress: 0
     },
 
     actions: {
@@ -18,15 +19,44 @@ export default new Vuex.Store({
             const token = await axios.post('api/auth/login', credentials)
             commit('SET_TOKEN', token)
         },
-        async ['UPLOAD_FILE'](context, formData) {
-            const {data} = await axios.post('api/files', formData)
-            return data
+        async ['UPLOAD_FILE']({commit}, formData) {
+            const onUploadProgress = (progressEvent) => {
+                const progress = parseInt(Math.round((progressEvent.loaded * 100) / progressEvent.total))
+                commit('SET_FILE_UPLOAD_PROGRESS', progress)
+            }
+            const options = {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                },
+                onUploadProgress: onUploadProgress
+            }
+
+            try {
+                commit('SET_LOADING')
+                commit('SET_FILE_UPLOAD_PROGRESS', 0)
+                const {data} = await axios.post('api/files', formData, options)
+                commit('SET_UNLOADING')
+                return data
+            } catch (err) {
+                commit('SET_FILE_UPLOAD_PROGRESS', 0)
+                commit('SET_UNLOADING')
+                throw err
+            }
         }
     },
 
     mutations: {
         ['SET_TOKEN'](state, token) {
             state.token = token
+        },
+        ['SET_FILE_UPLOAD_PROGRESS'](state, progress) {
+            state.fileUploadProgress = progress
+        },
+        ['SET_LOADING'](state) {
+            state.loading = true
+        },
+        ['SET_UNLOADING'](state) {
+            state.loading = false
         }
     }
 })
--- src/views/SendFile.vue
+++ src/views/SendFile.vue
@@ -5,11 +5,15 @@
                 <h2 class="display-1">Exame médico</h2>
                 <p>Para entrar com os dados de um exame médico basta clicar no botão abaixo e selecionar o arquivo que contém dos dados do exame</p>
                 <v-form autocomplete="off">
-                    <v-btn color="primary" v-if="!uploading" @click="onPickFile">Selecione um arquivo</v-btn>
+                    <v-btn color="primary" v-if="!loading" @click="onPickFile">Selecione um arquivo</v-btn>
                     <input ref="fileInput" @change="onFilePicked" type="file" style="display: none" accept="application/pdf">
-                    <v-progress-circular v-if="uploading" :size="100" :width="15" :rotate="360" :value="progress" color="teal">
-                        {{ progress }}
+                    <v-progress-circular v-if="loading" :size="100" :width="15" :rotate="360" :value="progress"
+                                         color="teal">
+                        {{ progress }} %
                     </v-progress-circular>
+                    <p v-if="uploadedFiledata">
+                        Parabens, o arquivo {{uploadedFiledata.filename}} foi carregado com sucesso
+                    </p>
                 </v-form>
             </v-layout>
         </v-slide-y-transition>
@@ -21,44 +25,30 @@
     export default {
         data() {
             return {
-                uploading: false,
-                progress: 0
+                uploadedFiledata: null
+            }
+        },
+        computed: {
+            loading() {
+                return this.$store.state.loading
+            },
+            progress() {
+                return this.$store.state.fileUploadProgress
             }
         },
         methods: {
             onPickFile() {
                 this.$refs.fileInput.click()
             },
-            onFilePicked(event) {
-                const file = event.target.files[0]
-                const formData = new FormData()
-                formData.append('file', file)
-
-                const reader = new FileReader()
-                reader.onerror = () => this.errorHandler(reader)
-                reader.onprogress = (evt) => this.updateProgressHandler(evt)
-
-                reader.onloadstart = () => {
-                    this.uploading = true
-                }
-
-                reader.onload = () => {
-                    this.progress = 100
-                    this.uploading = false
-                }
-                this.$store.dispatch('UPLOAD_FILE', formData)
-            },
-
-            errorHandler(reader) {
-                reader.abort()
-            },
-
-            updateProgressHandler(evt) {
-                if (evt.lengthComputable) {
-                    var percentLoaded = Math.round((evt.loaded / evt.total) * 100)
-                    if (percentLoaded < 100) {
-                        this.progress = percentLoaded + '%'
-                    }
+            async onFilePicked(event) {
+                try {
+                    const file = event.target.files[0]
+                    const formData = new FormData()
+                    formData.append('file', file)
+                    const filedata = await this.$store.dispatch('UPLOAD_FILE', formData)
+                    this.uploadedFiledata = filedata
+                } catch (err) {
+                    console.log('ERRO AO SUBIR ARQUIVO')
                 }
             }
         }
