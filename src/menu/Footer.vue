aa4c8d817f4a50150aabf7011733550cc850e766
Jhonatan Froeder <froeder3@gmail.com>
2018-06-21 14:11:18 -0400

    Separacao TOOLBAR, SIDEBAR E FOOTER

---
+++ src/menu/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-footer :fixed="fixed" app>
+        <span> PET-Sistemas UFMS & Living Lab &copy; 2018</span>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            fixed: false
+        }
+    }
+}
+</script>
+
