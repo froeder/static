cc4ea34b43e4b8f68712931ca9b372c7e5c6e4f6
Henrique Paulo Bonfim <40275173+hpbonfim@users.noreply.github.com>
2018-06-25 15:52:04 -0400

    Update alert error, sucess and loading bar

--- src/views/SendFile.vue
+++ src/views/SendFile.vue
@@ -6,24 +6,23 @@
                     <v-card-title>
                         <h1>Exame médico</h1>
                     </v-card-title>
+                    <div class="text-xs-center">
                     <v-card-text>Para entrar com os dados de um exame médico basta clicar no botão abaixo e selecionar o arquivo que contém dos dados do exame</v-card-text>
                     <v-form autocomplete="off">
                         <v-btn color="primary" v-if="!loading" @click="onPickFile">Selecione um arquivo</v-btn>
-                        <div class="text-xs-center">
                             <input ref="fileInput" @change="onFilePicked" type="file" style="display: none" accept="application/pdf">
-                        </div>
-                        <v-progress-circular v-if="loading" :size="100" :width="15" :rotate="360" :value="progress"
-                                            color="teal">
+                            <v-progress-circular v-if="loading" :size="100" :width="15" :rotate="360" :value="progress" color="teal">
                             {{ progress }} %
-                        </v-progress-circular>
-                        <v-alert :value="true" type="success" v-if="uploadedFiledata">
-                            Parabens, o arquivo {{uploadedFiledata.filename}} foi carregado com sucesso
-                        </v-alert>
-                        <v-alert v-if="errorUpload" :value="true" type="error">
+                            </v-progress-circular>
+                            <v-alert v-if="uploadedFiledata" v-model="alert" type="success" dismissible>
+                                 Parabens, o arquivo {{uploadedFiledata.filename}} foi carregado com sucesso
+                            </v-alert>
+                            <v-alert v-if="errorUpload" v-model="alert" type="error" dismissible>
                             O arquivo nao foi enviado
-                        </v-alert>
+                            </v-alert>
                     </v-form>
-                </v-card>
+                  </div>
+               </v-card>
             </v-layout>
         </v-slide-y-transition>
     </v-container>
@@ -36,6 +35,7 @@
     export default {
         data() {
             return {
+                alert: true,
                 uploadedFiledata: null,
                 errorUpload: false,
                 user_logged: ''
