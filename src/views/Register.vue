19826bfb981f1e8dcf261a58490a462e8aef698b
Unknown <diegobmoraes2@gmail.com>
2018-07-01 16:34:00 -0400

    Email verification inclusion
    Inclusion of the email verification if it is already in the database

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -10,7 +10,7 @@
                         <v-form  ref='form' v-model="valid" lazy-validation autocomplete="off">
                             <v-text-field @change="check_email" v-model="user.email" name="email" v-validate="'required|email'" :error-messages="errors.collect('email')" id="email" label="E-mail" prepend-icon="email" type="text" 
                                           required></v-text-field>
-                            <v-text-field v-model="user.password" name="password" v-validate="'required|min:6'" :error-messages="errors.collect('password')"  label="Senha" prepend-icon="lock"
+                            <v-text-field @change="check_password" v-model="user.password" name="password" v-validate="'required|min:6'" :error-messages="errors.collect('password')"  label="Senha" prepend-icon="lock"
                                           type="password"></v-text-field>
                             <v-text-field @change="check_password" v-model="confirmed_password" name="password_confirmation" v-validate="'required|min:6'" :error-messages="errors.collect('password_confirmation')" label="Confirma a senha" prepend-icon="lock"
                                           type="password"></v-text-field>
@@ -18,10 +18,10 @@
                                 <v-btn color="info" :large="true" to="/">Voltar</v-btn>
                                 <v-btn color="success" :large="true" :disabled="!valid" @click="validateBeforeSubmit">SALVAR</v-btn>
                             </div>
-                            <v-alert v-if="valid_email" :value="false" type="error">
+                            <v-alert v-model="alert_email" type="error">
                                O email já esta cadastrado
                             </v-alert>
-                            <v-alert v-if="valid_password" :value="false" type="error">
+                            <v-alert v-model="alert_password" type="error">
                                 Senhas não conferem
                             </v-alert>
                         </v-form>
@@ -34,17 +34,16 @@
 
 <script>
     /* eslint-disable no-console */
-    import events from '../events'
-
     export default {
         $_veeValidate: {
             validator: 'new'
         },
         data: () => ({
             valid_email: false,
-            valid_password: false,
             valid: false,
             confirmed_password: '',
+            alert_password: false,
+            alert_email: false,
             user: {
                 email: '',
                 password: ''
@@ -90,7 +89,13 @@
             },
             async check_password() {
                 try {
-                    this.valid_password = this.confirmed_password == this.user.password;
+                    var valid_password = this.confirmed_password == this.user.password
+                    if(!valid_password && !this.confirmed_password == "" && !this.user.password == ""){
+                        this.alert_password = true
+                    }else{
+                        this.alert_password = false
+                        this.valid_password = true
+                    }
                 } catch (err) {
                     console.log(err)
                 }
@@ -98,6 +103,12 @@
             async check_email() {
                 try {
                     this.check_email = !(await this.$store.dispatch('CHECK_EMAIL', this.user.email)).data.exist
+                    if(!this.check_email){
+                        this.alert_email = true
+                    }else{
+                        this.alert_email = false
+                        this.valid_email = true
+                    }
                 } catch (err) {
                     console.log(err)
                 }
